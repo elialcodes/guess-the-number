338433de75e4454c0abeee99919672f6049738fa
Noelia <abascalcabreran@gmail.com>
2024-02-22 09:06:00 +0100

    const created

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 'use strict';
+
+const inputNumber = documen.querySelector('.js-inputNumber');
+const buttonTest = document.querySelector('.js-buttonTest');
+const inputHint = document.querySelector('.js-inputHint');
+const inputTried = document.querySelector('.js-inputTried');
